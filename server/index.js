cd240e6332c0bcc2130c0afe7de2987801ec76df
Nathan <n.hatz@yahoo.com>
2018-05-22 16:52:08 -0500

    All Steps Finished/ Postman: No Errors

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const massive = require("massive");
 require("dotenv").config();
 
+const products_controller = require("./controllers/products_controller");
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -11,6 +13,12 @@ app.use(cors());
 massive(process.env.CONNECTION_STRING).then(dbInstance =>
   app.set("db", dbInstance)
 );
+
+app.get("/api/products", products_controller.getAll);
+app.get("/api/product/:id", products_controller.getOne);
+app.put("/api/product/:id?desc=...", products_controller.update);
+app.post("/api/product", products_controller.create);
+app.delete("/api/products/:id", products_controller.delete);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
